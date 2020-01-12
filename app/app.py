c7b7dd2ea7c9d8c2262145694ec77b5357b96bc6
Eleni Kadi <kadi.eleni@gmail.com>
2020-01-12 02:49:21 +0200

    My code so far <ek>

--- app/app.py
+++ app/app.py
@@ -1,8 +1,22 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 
-@app.route('/hello')
+@app.route('/query')
 def hello():
-    return 'Hello Eleni'
+    if request.args:
+    
+        # We have our query string nicely serialized as a Python dictionary
+        args = request.args
+
+        # We'll create a string to display the parameters & values (???)
+        serialized = ", ".join(f"{k}: {v}" for k, v in request.args.items())
+
+        # Display the query string to the client in a different format
+        return f"Hello {serialized}", 200
+
+    else:
+
+        return "No query string received", 200 
