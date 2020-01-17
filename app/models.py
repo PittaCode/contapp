7f2514207518d8fc6a82d474ad60731ff164773e
Andreas Ανδρέου <andreas.andreou.dev@gmail.com>
2020-01-17 21:54:08 +0000

    [AA] Add initial migration and database
    Add user table with initial columns
    Will change
    
    KISS

---
+++ app/models.py
@@ -0,0 +1,11 @@
+from app import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), index=True)
+    surname = db.Column(db.String(128), index=True)
+    alias = db.Column(db.String(128), index=True)
+
+    def __repr__(self):
+        return f'<User {self.name} {self.surname} AKA {self.alias}>'
