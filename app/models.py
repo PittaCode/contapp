1fced3c53dce7a14a8dd8a11b18c1dd1c8595293
Andreas Ανδρέου <andreas.andreou.dev@gmail.com>
2020-01-18 10:43:35 +0000

    [AA] Alter alias to be unique

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), index=True)
     surname = db.Column(db.String(128), index=True)
-    alias = db.Column(db.String(128), index=True)
+    alias = db.Column(db.String(128), index=True, unique=True)
 
     def __repr__(self):
         return f'<User {self.name} {self.surname} AKA {self.alias}>'
