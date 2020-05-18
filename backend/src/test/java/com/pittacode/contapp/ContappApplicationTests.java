ed1952b0f9c7ea0bb87a25942237be094b65ed91
Andreas Ανδρέου <andreas.andreou.dev@gmail.com>
2020-05-18 16:34:11 +0100

    [AA] Init java spring boot project

---
+++ backend/src/test/java/com/pittacode/contapp/ContappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pittacode.contapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
