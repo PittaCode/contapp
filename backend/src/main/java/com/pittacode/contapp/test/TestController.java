d0f0e64f127047c03c3c5f9de7fb71a7283e8849
Andreas Ανδρέου <andreas.andreou.dev@gmail.com>
2020-05-22 13:12:10 +0100

    [AA] Make test controller to show case url params

---
+++ backend/src/main/java/com/pittacode/contapp/test/TestController.java
@@ -0,0 +1,23 @@
+package com.pittacode.contapp.test;
+
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@RestController
+@RequestMapping(path = "/test")
+public class TestController implements BeanPostProcessor {
+
+
+    @RequestMapping(path = "/ping", method = GET)
+    public String greeting(@RequestParam(required = true, name = "name") String name, @RequestParam(required = false, name = "surname") String lastName) {
+        return lastName == null
+                ? "Hello " + name
+                : "Hello " + name + " " + lastName;
+    }
+}
+
+
