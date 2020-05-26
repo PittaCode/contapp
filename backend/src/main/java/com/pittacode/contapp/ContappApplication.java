105c63c53f0617985ba5c437ee04bd40317abdcf
Andreas Ανδρέου <andreas.andreou.dev@gmail.com>
2020-05-26 13:04:37 +0100

    [AA] Showcase XML di with external jar
    Using XML configuration to inject beans

--- backend/src/main/java/com/pittacode/contapp/ContappApplication.java
+++ backend/src/main/java/com/pittacode/contapp/ContappApplication.java
@@ -2,8 +2,10 @@ package com.pittacode.contapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:joke-configuration.xml")
 public class ContappApplication {
 
     public static void main(String[] args) {
